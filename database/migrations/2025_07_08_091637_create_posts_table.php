67aa4529722a02e52289b8f8f380ccfae2a5da3e
ErvinTyx <ervinteohyx@gmail.com>
2025-07-08 22:46:28 +0800

    Complete blueprint for table category and post
    table are form and migrated

--- database/migrations/2025_07_08_091637_create_posts_table.php
+++ database/migrations/2025_07_08_091637_create_posts_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('image')->nullable();
+            $table->string('tittle');
+            $table->string('slug')->unique();
+            $table->longText('content');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');// constrained means if category is does not exists then it would not create, onDelete means if category deleted all of its post will be deleted
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamp('published_at')->nullable();// nullable means allow to be empty
             $table->timestamps();
         });
     }
---
+++ refence.md
@@ -0,0 +1,31 @@
+### Artisan command
+`php artisan db:table`
+to inspect table
+
+
+`php artisan make:model -m <name_of_model> `
+is to make the blueprints for the database
+
+`php artisan make:model -m <name_of_model> -f`
+is to make the blueprints for the database and make factory
+
+if make the blueprints but dont have model 
+`php artisan make:factory <name_of_model>Factory`
+
+`php artisan migrate`
+is to run the database migration (aka save or commit)
+
+`php artisan migrate:rollback --step=<n>`
+is to undo the changes to the database that save, n means the number times to revert 
+
+
+`php artisan tinker`
+interact with the database
+
+### Tinker
+```bash
+\App\Models\Category::get()
+```
+is to view values in the category table
+
+
