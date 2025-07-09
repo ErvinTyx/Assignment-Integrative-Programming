58ba478714f515ec8593a58690bdf51f31a0a5f4
ErvinTyx <ervinteohyx@gmail.com>
2025-07-09 23:22:06 +0800

    Added route for PostController

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard',[PostController::class,'index'])->middleware(['auth','verified'])->name('dashboard');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
