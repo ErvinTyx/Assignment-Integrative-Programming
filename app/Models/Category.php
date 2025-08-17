62630d1e31af903bec8f26025e936a5119c43dce
ErvinTyx <ervinteohyx@gmail.com>
2025-08-17 13:08:03 +0800

    Manage categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     //
+    public function parent()
+    {
+        return $this->belongsTo(Category::class,"parent_id");
+    }
+
+
 }
---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            [
+                "name" => "Electronics",
+                'department_id' => 1,
+                'parent_id' => null,
+                'active' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Fashion',
+                'department_id' => 2,
+                'parent_id' => null,
+                'active' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                "name" => "Computers",
+                "department_id" => 1,
+                "parent_id" => null,
+                "active" => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Smartphones',
+                'department_id' => 1,
+                'parent_id' => 1,
+                'active' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ];
+
+        \Illuminate\Support\Facades\DB::table('categories')->insert($categories);
+    }
+}
