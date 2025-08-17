0738292fa6c3b2077dc359d2ff7e5bf4cfca0229
ErvinTyx <ervinteohyx@gmail.com>
2025-08-17 16:33:15 +0800

    CRUD product

---
+++ database/migrations/2025_08_17_050959_create_products_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 200);
+            $table->string('slug', 200);
+            $table->text('description');
+            $table->foreignId('department_id')
+                ->index()
+                ->constrained();
+            $table->foreignId('category_id')
+                ->index()
+                ->constrained('categories');
+            $table->decimal('price', 20, 2);
+            $table->string('status')
+                ->index();
+            $table->integer('quantity')->nullable();
+            $table->foreignIdFor(User::class, 'created_by');
+            $table->foreignIdFor(User::class,'updated_by');
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
